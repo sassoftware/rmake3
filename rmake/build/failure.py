86891d4c8a1e1e24d9dae2ece7fd32c9420673fd
David Christian <http://issues.rpath.com>
2006-09-27 15:56:21 -0400

    add missing parens

--- rmake/build/failure.py
+++ rmake/build/failure.py
@@ -69,10 +69,10 @@ class FailureWithException(FailureReason):
 
     def __init__(self, error='', traceback=''):
         if isinstance(error, (list, tuple)):
-            assert(isinstance(error[0], str)
+            assert(isinstance(error[0], str))
             FailureReason.__init__(self, list(error))
         else:
-            assert(isinstance(error, str)
+            assert(isinstance(error, str))
             FailureReason.__init__(self, [error, traceback])
 
     def getErrorMessage(self):
